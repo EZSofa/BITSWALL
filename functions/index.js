3ed9751e90633f4837fe503f88402975b8908f09
Noah Chou <noah.chou@eztable.com>
2019-11-26 22:41:09 +0800

    API of Health and BitsWalls

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,28 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const bodyParser = require('body-parser');
+const express = require('express');
+const cors = require('cors');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const app = express();
+
+admin.initializeApp();
+
+// Automatically allow cross-origin requests
+app.use(cors({ origin: true }));
+app.use(bodyParser.json());
+
+app.post('/health', async (req, res) => {
+    res.json({status:'OK'});
+});
+
+// build multiple CRUD interfaces:
+app.post('/:channelId', async (req, res) => {
+    const channelId = req.params.channelId;
+    const bitsWall = req.body.bitsWall;
+    await admin.database().ref(`/bitsWalls/${channelId}`).set(bitsWall);
+    res.json();
+});
+
+// Expose Express API as a single Cloud Function:
+exports.bitsWalls = functions.https.onRequest(app);
